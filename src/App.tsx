2d60893d8b7022b7de2443184e252baf4de37906
Akshat <iamakku0.0.0.1@gmail.com>
2024-11-05 14:12:57 +0530

    auth update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   const isConnected = useHMSStore(selectIsConnectedToRoom);
   const hmsActions = useHMSActions();
 
-  const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3Nfa2V5IjoiNjcxNjg2OWEzM2NlNzRhYjliZTk0M2Y0Iiwicm9vbV9pZCI6IjY3MWYzMzY2M2UyNjRlNzI1YzdkNjNmMyIsInVzZXJfaWQiOiIxIiwicm9sZSI6ImRvY3RvciIsInR5cGUiOiJhcHAiLCJ2ZXJzaW9uIjoyLCJpYXQiOjE3MzAwOTgzOTMsIm5iZiI6MTczMDA5ODM5Mywic3ViIjoiYXBpIiwiZXhwIjoxNzMwMTg0NzkzLCJqdGkiOiJmNjAyMWUzYS1iZTJhLTQ3ODEtYjkxOC0wMWU3YTMwNDZmMTYifQ.Nw81-Z7zwfiaIxg1hVN3hU7M8NQPArz-bMAAhHy4oqk"; // Replace this with your actual token
+  const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3Nfa2V5IjoiNjcxNjg2OWEzM2NlNzRhYjliZTk0M2Y0Iiwicm9vbV9pZCI6IjY3MjlkOGQ1M2UyNjRlNzI1YzdlYjg1MCIsInVzZXJfaWQiOiIxMCIsInJvbGUiOiJkb2N0b3IiLCJ0eXBlIjoiYXBwIiwidmVyc2lvbiI6MiwiaWF0IjoxNzMwNzk1NzMzLCJuYmYiOjE3MzA3OTU3MzMsInN1YiI6ImFwaSIsImV4cCI6MTczMDg4MjEzMywianRpIjoiMDI1NzNlNDUtZDA1Zi00YjgzLThjMmUtMTRmOTQzOGNhZjFlIn0.CsT7i1LqDdFDQKK-v9a6vWqdk-2OI1RgriI3eM7gtCg"; // Replace this with your actual token
 
   useEffect(() => {
     window.onunload = () => {
